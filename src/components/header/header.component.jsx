5af05000fee5a0ffb4d103780fe5fd5b267355ab
Nikola <milje18@gmail.com>
2020-08-29 10:49:16 +0200

    state moved to firebase and redux implemented with higher order components

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebase.utils'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.scss'
+// import './header.styles.scss'
 
 import { connect } from 'react-redux'
 import CartIcon from '../cart-icon/cart-icon.component'
@@ -10,20 +9,22 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import { createStructuredSelector } from 'reselect'
 import { selectCartHidden } from '../../redux/cart/cart.selectors'
 import { selectCurrentUser } from '../../redux/user/user.selectors'
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink} from './header.styles'
+
 
 const Header = ({ currentUser, hidden }) => (
-    <div className="header">
-        <Link className="logo-container" to="/">
+    <HeaderContainer>
+        <LogoContainer to="/">
             <Logo className="logo"/>
-        </Link>
-        <div className="options" to="/shop">
-            <Link className="option" to="/shop">SHOP</Link>
-            <Link className="option" to="/shop">CONTACT</Link>
-            {currentUser ? <div onClick={()=> auth.signOut()}>SIGN OUT</div> : <Link className="option" to="/signin">SIGN IN</Link> }
+        </LogoContainer>
+        <OptionsContainer>
+            <OptionLink to="/shop">SHOP</OptionLink>
+            <OptionLink to="/shop">CONTACT</OptionLink>
+            {currentUser ? <OptionLink as='div' onClick={()=> auth.signOut()}>SIGN OUT</OptionLink> : <OptionLink to="/signin">SIGN IN</OptionLink> }
             <CartIcon/>
-        </div>
+        </OptionsContainer>
         {hidden ? null : <CartDropdown/>}
-    </div>
+    </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -17,7 +17,9 @@ const config = {
       if(!userAuth) return;
 
       const userRef = firestore.doc(`users/${userAuth.uid}`);
+      
       const snapShot = await userRef.get();
+
       
       if(!snapShot.exists){
           const {displayName, email } = userAuth;
@@ -40,6 +42,35 @@ const config = {
 
 
   firebase.initializeApp(config);
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+      const collectionRef = firestore.collection(collectionKey);
+
+      const batch = firestore.batch();
+      objectsToAdd.forEach(obj => {
+          const newDocRef = collectionRef.doc();
+          batch.set(newDocRef,obj);
+      });
+      return await batch.commit();
+  };
+
+  export const convertCollectionsSnapshotToMap = collections => {
+    const transformedCollection = collections.docs.map(doc => {
+      const { title, items } = doc.data(); 
+      return {
+        routeName: encodeURI( title ? title.toLowerCase() : ''),
+        id: doc.id,
+        title,
+        items
+      };
+    });
+
+    return transformedCollection.reduce((accumulator, collection)=>{
+        accumulator[collection.title ? collection.title.toLowerCase() : ''] = collection;
+        return accumulator;
+    },{});
+
+}
 
   export const auth = firebase.auth();
   export const firestore = firebase.firestore();
